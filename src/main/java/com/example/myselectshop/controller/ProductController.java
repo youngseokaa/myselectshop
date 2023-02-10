7c706fc850b1929b2dceda965bd41442ce85cc99
jenny <jennykim6890@naver.com>
2023-02-10 23:04:16 +0900

    라운드

---
+++ src/main/java/com/example/myselectshop/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.example.myselectshop.controller;
+
+import com.example.myselectshop.dto.ProductMypriceRequestDto;
+import com.example.myselectshop.dto.ProductRequestDto;
+import com.example.myselectshop.dto.ProductResponseDto;
+import com.example.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    // 관심 상품 등록하기
+
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, HttpServletRequest request) {
+        // 응답 보내기
+        return productService.createProduct(requestDto, request);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts(HttpServletRequest request) {
+        // 응답 보내기
+        return productService.getProducts(request);
+    }
+
+
+    // 관심 상품 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto, HttpServletRequest request) {
+        // 응답 보내기 (업데이트된 상품 id)
+        return productService.updateProduct(id, requestDto, request);
+    }
+
+}
