190cbc70640f1972b1038a53dc8bdf9d52c89024
jenny <jennykim6890@naver.com>
2023-02-17 21:57:41 +0900

    라운드

--- src/main/java/com/example/myselectshop/controller/ProductController.java
+++ src/main/java/com/example/myselectshop/controller/ProductController.java
@@ -3,12 +3,15 @@ package com.example.myselectshop.controller;
 import com.example.myselectshop.dto.ProductMypriceRequestDto;
 import com.example.myselectshop.dto.ProductRequestDto;
 import com.example.myselectshop.dto.ProductResponseDto;
+import com.example.myselectshop.entity.Product;
+import com.example.myselectshop.entity.UserRoleEnum;
+import com.example.myselectshop.security.UserDetailsImpl;
 import com.example.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,27 +21,42 @@ public class ProductController {
     private final ProductService productService;
 
     // 관심 상품 등록하기
-
-    // 관심 상품 등록하기
+//    @Secured(UserRoleEnum.Authority.ADMIN)
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, HttpServletRequest request) {
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기
-        return productService.createProduct(requestDto, request);
+        return productService.createProduct(requestDto, userDetails.getUser());
     }
 
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(HttpServletRequest request) {
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         // 응답 보내기
-        return productService.getProducts(request);
+        return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
-
 
     // 관심 상품 최저가 등록하기
     @PutMapping("/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto, HttpServletRequest request) {
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기 (업데이트된 상품 id)
-        return productService.updateProduct(id, requestDto, request);
+        return productService.updateProduct(id, requestDto, userDetails.getUser());
+    }
+
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public Long addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        Product product = productService.addFolder(productId, folderId, userDetails.getUser());
+        return product.getId();
     }
 
 }
--- src/main/java/com/example/myselectshop/service/UserService.java
+++ src/main/java/com/example/myselectshop/service/UserService.java
@@ -6,28 +6,29 @@ import com.example.myselectshop.entity.User;
 import com.example.myselectshop.entity.UserRoleEnum;
 import com.example.myselectshop.jwt.JwtUtil;
 import com.example.myselectshop.repository.UserRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
-@Setter
-@Getter
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
     // ADMIN_TOKEN
     private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Transactional
     public void signup(SignupRequestDto signupRequestDto) {
         String username = signupRequestDto.getUsername();
-        String password = signupRequestDto.getPassword();
+        String password = passwordEncoder.encode(signupRequestDto.getPassword());
 
         // 회원 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
@@ -47,11 +48,7 @@ public class UserService {
 
         User user = new User(username, password, email, role);
         userRepository.save(user);
-
     }
-
-
-
 
     @Transactional(readOnly = true)
     public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
@@ -62,8 +59,9 @@ public class UserService {
         User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
         );
+
         // 비밀번호 확인
-        if(!user.getPassword().equals(password)){
+        if(!passwordEncoder.matches(password, user.getPassword())){
             throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
